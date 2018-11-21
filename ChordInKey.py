b0080f12c2ca634d8454f5069b98dd73ad627018
TheXenonPhoenix <majorgamerkevin@gmail.com>
2018-11-21 16:23:05 -0600

    notesInScale method now accepts all modes and keys

--- ChordInKey.py
+++ ChordInKey.py
@@ -3,19 +3,20 @@ from collections import deque
 
 notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
 
+# Not Necessary, but helpful for readablilty
+#Major       = {'Major':(0,2,4,5,7,9,11)}
+#Dorian      = {'Dorian':(0,2,3,5,7,9,10)}
+#Phrygian    = {'Phrygian':(0,1,3,5,7,8,10)}
+#Lydian      = {'Lydian':(0,2,4,6,7,9,11)}
+#Mixolydian  = {'Mixolydian':(0,2,4,5,7,9,10)}
+#Minor       = {'Minor':(0,2,3,5,7,8,10)}
+#Locrian     = {'Locrian':(0,1,3,5,6,8,10)}
 
-Major       = (0,2,4,5,7,9,11)
-Dorian      = (0,2,3,5,7,9,10)
-Phrygian    = (0,1,3,5,7,8,10)
-Lydian      = (0,2,4,6,7,9,11)
-Mixolydian  = (0,2,4,5,7,9,10)
-Minor       = (0,2,3,5,7,8,10)
-Locrian     = (0,1,3,5,6,8,10)
+# Creates a dictionary containging how to build each key
+modes = {'Major':(0,2,4,5,7,9,11),'Dorian':(0,2,3,5,7,9,10),'Phrygian':(0,1,3,5,7,8,10),'Lydian':(0,2,4,6,7,9,11),'Mixolydian':(0,2,4,5,7,9,10),'Minor':(0,2,3,5,7,8,10),'Locrian':(0,1,3,5,6,8,10)} 
 
-modes = ['Major','Dorian','Phrygian','Lydian','Mixolydian','Minor','Locrian'] # hardcoded to just work with major
-
-modestest = {'Major':(0,2,4,5,7,9,11)}
-
+# 
+# Returns the notes of the mode starting at the root note
 def notesInScale(mode, shiftKeys):
     scale = []
     for step in mode:
@@ -35,17 +36,14 @@ def userInput(str, lst):
 
 key = userInput("Key", notes)
 mode = userInput("Mode", modes)
-print("Here are the chords in " + key, mode)
 
+print("Here are the chords in " + key, mode)
 
 shiftKeys = deque(notes)         # Allows you to change the root notes of the key
 shiftVal = notes.index(key)      # Gets the amount that the notes have to be shifted by
 shiftKeys.rotate(-shiftVal)     # Shifts the notes to get correct root note
 
-
-print(mode)
-
-scale = notesInScale(Minor, shiftKeys) 
+scale = notesInScale(modes.get(mode), shiftKeys) 
 
 for count in range(len(scale)):
     chord = (scale[(count+0)%len(scale)], scale[(count+2)%len(scale)], scale[(count+4)%len(scale)])
