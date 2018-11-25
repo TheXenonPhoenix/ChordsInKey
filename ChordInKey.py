cdbda31da3d3004b79c2b82adb2fbb00a60040a3
TheXenonPhoenix <majorgamerkevin@gmail.com>
2018-11-25 16:45:25 -0600

    all modes now work properly

--- ChordInKey.py
+++ ChordInKey.py
@@ -16,6 +16,7 @@ notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
 
 # Creates a dictionary containging how to build each key
 modes = {'Major':(0,2,4,5,7,9,11),'Dorian':(0,2,3,5,7,9,10),'Phrygian':(0,1,3,5,7,8,10),'Lydian':(0,2,4,6,7,9,11),'Mixolydian':(0,2,4,5,7,9,10),'Minor':(0,2,3,5,7,8,10),'Locrian':(0,1,3,5,6,8,10)} 
+dims = {'Major':6,'Dorian':5,'Phrygian':4,'Lydian':3,'Mixolydian':2,'Minor':1,'Locrian':0} 
 
 fifth = 7
 
@@ -51,8 +52,8 @@ def findSecondaryDominates(shiftedNotes, scale, dim):
 
 def main():
     key = userInput("Key", notes)
-    mode = "Major"                  # userInput("Mode", modes) ##### Commenting out to run on alpha version
-
+    mode = userInput("Mode", modes) ##### Commenting out to run on alpha version
+    
     print("Here are the chords in " + key, mode)
 
     shiftKeys = deque(notes)        # Allows you to change the root notes of the key
@@ -67,8 +68,7 @@ def main():
 
     shiftedNotes = list(shiftKeys)
 
-    dim = 6
-    findSecondaryDominates(shiftedNotes, scale, dim)
+    findSecondaryDominates(shiftedNotes, scale, dims.get(mode))
     
 main()
 #time.sleep(10)
