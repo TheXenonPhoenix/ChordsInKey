37332b8f7475ccbc02973a8c6dc0c1e89a6fe1c0
maxmccomb <maxwellmccomb16@augustana.edu>
2018-12-04 13:13:54 -0600

    added quotes around dates

--- classes/GUI.py
+++ classes/GUI.py
@@ -7,27 +7,48 @@ class MusicApp(wx.Frame):
 
     def __init__(self, *args, **kw):
         super(MusicApp, self).__init__(*args, **kw)
-
         self.InitUI()
 
     def InitUI(self):
 
         pnl = wx.Panel(self)
-        submitButton = wx.Button(pnl, label='Close', pos=(150, 150))
-        noteCombo = wx.ComboBox(pnl, pos=(75, 10), choices=chordObj.notes, 
-            style=wx.CB_READONLY)
-        cb = wx.ComboBox(pnl, pos=(200, 10), choices=chordObj.typeChords, 
-            style=wx.CB_READONLY)
 
-        submitButton.Bind(wx.EVT_BUTTON, self.OnSubmit)
+        #font = wx.Font(13, wx.DEFAULT, wx.NORMAL, wx.BOLD)
 
-        self.SetSize((350, 250))
-        self.SetTitle('Chords')
+        title = wx.StaticText(pnl, label='Notes in Key',pos = (50,10), style=wx.ALIGN_LEFT)
+        
+        #title.SetFont(font)
+
+        submitButton = wx.Button(pnl, label='Submit', pos=(50, 100))
+
+        noteCombo = wx.ComboBox(pnl, pos=(25, 50), choices=chordObj.notes, style=wx.CB_READONLY)
+        keyCombo = wx.ComboBox(pnl, pos=(100, 50), choices=chordObj.typeChords, style=wx.CB_READONLY)
+
+        submitButton.Bind(wx.EVT_BUTTON, self.OnSubmit(noteCombo))
+
+
+        self.SetSize((750, 300))
+        self.SetTitle('Sing Along with Kevin')
         self.Centre()
 
     def OnSubmit(self, e):
+        noteSelection = self.noteCombo.GetValue()
+        keySelection = self.keyCombo.GetValue()
+        print(noteSelection)
+        print(keySelection)
+        #secondWindow = DisplayWindow()
 
-        self.Close(True)
+
+    
+# class DisplayWindow():
+#     def __init__(self, *args, **kw):
+#         super(DisplayWindow, self).__init__(*args, **kw)
+#         self.InitUI()
+
+#     def InitUI(self):
+#         disPnl = wx.Panel(self)
+#         self.Show()
+
 
 class GUIClass():
     def GUImain(self):
